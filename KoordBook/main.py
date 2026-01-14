fe2aecda4b29ec3423d087b3ef8a246d404af7d9
Lyubo Yuriy <uralubo@gmail.com>
2026-01-14 18:36:35 +0300

    Update main.py

--- KoordBook/main.py
+++ KoordBook/main.py
@@ -115,7 +115,7 @@ class GoogleSheetsManager:
             "auth_uri": "https://accounts.google.com/o/oauth2/auth",
             "token_uri": "https://oauth2.googleapis.com/token",
             "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/telegram-bot-sheets%40koordbook.iam.gserviceaccount.com"
+            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/telegram-bot-sheets%40koordbook.iam.gserviceaccount.com",
             "universe_domain": "googleapis.com"
         }
 
@@ -730,4 +730,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
